c37895661d14c6adb5954c20c654b9caa6e81dbb
Euak-Euak <79529743+Euak-Euak@users.noreply.github.com>
2023-07-04 16:23:49 +0900

    Design Patterns
    디자인 패턴 (싱글톤, 오브젝트 풀링) 업로드

---
+++ ShootingRhythmGame/Assets/Scripts/Singleton.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+/// <summary>
+/// 싱글톤. 상속을 통해 이루어짐
+/// </summary>
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    // 본인
+    private static T _instance = null;
+
+    //값 가져오기 (보안을 위한 속성)
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+                return null;
+
+            return _instance;
+        }
+    }
+
+    // 초기화
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = GetComponent<T>();
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+    }
+}
