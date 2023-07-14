57df05910cde82aee348545570c072744d0fa1a1
Euak-Euak <79529743+Euak-Euak@users.noreply.github.com>
2023-07-14 10:07:19 +0900

    Shop Enter Animation
    상점 입장 애니메이션 및 최종 화면 복원 중...

--- ShootingRhythmGame/Assets/Scripts/Monster/MonsterManager.cs
+++ ShootingRhythmGame/Assets/Scripts/Monster/MonsterManager.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class MonsterManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+//public class MonsterManager : ObjectPooling<Monster>
+//{
+//    EnemyGameData _enemyGameData;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+//    [SerializeField]
+//    private List<Transform> _transform;
+
+//    [SerializeField]
+//    private List<GameObject> _monsters;
+
+//    public void Init(EnemyGameData data)
+//    {
+//        _enemyGameData = data;
+//        StartCoroutine(GameStart());
+//    }
+
+//    public IEnumerator GameStart()
+//    {
+//        WaitForSecondsRealtime wait = new WaitForSecondsRealtime(1f);
+
+//        foreach (SecondEnemyData data in _enemyGameData.EnemyDatas)
+//        {
+//            foreach (EnemyData enemyData in data.EnemyDatas)
+//            {
+//                Monster monster = SpawnObject(_monsters[(int)enemyData.EnemyType]);
+//                monster.Init(enemyData);
+//                monster.Move(_transform[enemyData.StartPosition - 1].position, _transform[enemyData.EndPosition - 1].position);
+//            }
+//            yield return wait;
+//        }
+//    }
+//}
+
